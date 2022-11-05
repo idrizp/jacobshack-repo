69b92408e685783660099fd9524e221f64f2a29a
JonLumi <jonnlumii@gmail.com>
2022-11-05 13:29:01 +0100

    pergenin

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello GENII</p>"
+page = 1
 
+person = [
+    {"name": "userName", "points": 100}
+]
+
+leaderboard = [
+    {"page": page, "persons": person}
+]
+
+@app.route("/leaderboard/{page}")
+def get_leaderbaord():
+    return jsonify(leaderboard)
+
+@app.route("/leaderboard", methods=['POST'])
+def add_leaderbaord():
+
+    persons.append(request.get_json())
+
+    return '', 204
