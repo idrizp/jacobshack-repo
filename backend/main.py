885c5595ac1818224cc9e407f32bf8c8818c0ba4
idrizp <idrizpelaj@hotmail.com>
2022-11-05 15:24:56 +0100

    Removed broken main

--- backend/main.py
+++ backend/main.py
@@ -4,34 +4,6 @@ import endpoint
 
 app = Flask(__name__)
 
-page = 1
-
-persons = [
-    {'name' : 'name', 'points' : 100}
-]
-
-leaderboard = [
-    {'page' : page, 'persons' : persons}
-]
-
-
-@app.route("/leaderboard")
-def get_leaderboard():
-@app.route("/leaderboard/<page>")
-def get_leaderboard(page):
-    return jsonify(leaderboard)
-
-# done by: uncrownedking1
-# route to get a name and image as input
-@app.route("/leaderboard/entry", methods = 'POST')
-def entry():
-    # getting input with name = name in HTML form
-    name = request.form.get("name")
-    # getting input with image = image in HTML form
-    image = request.files.get("image")
-    # returning name and image side-by-side
-    return name + image
-
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
