a6fe2770b2e8b38ad4f29380f381499b5868b7f2
UncrownedKing1 <108270526+UncrownedKing1@users.noreply.github.com>
2022-11-05 15:15:46 +0100

    added the route for getting input

--- backend/main.py
+++ backend/main.py
@@ -12,11 +12,20 @@ leaderboard = [
     {'page' : page, 'persons' : persons}
 ]
 
+
 @app.route("/leaderboard")
 def get_leaderboard():
     return jsonify(leaderboard)
 
-
+# done by: uncrownedking1
+# route to get a name and image as input
+@app.route("/leaderboard/entry", methods = 'POST')
+def entry():
+    # getting input with name = fname in HTML form
+    name = request.form.get("name")
+    # getting input with name = lname in HTML form
+    image = request.files.get("image")
+    return "Your name is "+ name + image
 
 @app.teardown_appcontext
 def close_connection(exception):
